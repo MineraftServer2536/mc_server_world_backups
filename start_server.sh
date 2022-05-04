5ea42b44524547a05fca67eb662d8d25bf10fb39
root <root@ip-172-31-3-37.us-west-2.compute.internal>
2022-05-04 03:00:03 +0000

    Auto Backup

--- start_server.sh
+++ start_server.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
-while true
-do
 java -Xmx7168M -Xms7168M -jar server.jar nogui
-echo "If you want to completely stop the server process now, press Ctrl+C before
-the time is up!"
-/home/ubuntu/mc/discord.sh/discord.sh --text "嗚嗚伺服醬撐不住了崩潰了喇... 馬上在兩分鐘內努力重起達QAQ"
-echo "Rebooting in:"
-for i in 5 4 3 2 1
-do
-echo "$i..."
-sleep 1
-done
-echo "Rebooting now!"
-done 
+echo "MC Server Stop"
+/home/ubuntu/mc/discord.sh/discord.sh --text "程式停止啦，實體醬準備要關機下班嚕~"
