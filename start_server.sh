eacac54bd73f6054bb0b60bf0d183e3159bb48f3
root <root@ip-172-31-3-37.us-west-2.compute.internal>
2022-05-04 03:46:51 +0000

    Auto Backup

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 java -Xmx7168M -Xms7168M -jar server.jar nogui
 echo "MC Server Stop"
-/home/ubuntu/mc/discord.sh/discord.sh --text "程式停止啦，實體醬準備要關機下班嚕~"
+/home/ubuntu/mc/discord.sh/discord.sh --text "程式已經停止啦，實體醬關機下班嚕~"
+sudo shutdown
